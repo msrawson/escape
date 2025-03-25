a6100f3767c428a92a397ce0db4bfb559309ef54
Mike Rawson <33514027+msrawson@users.noreply.github.com>
2025-03-25 22:17:46 +0000

    update screen resolution to regular HD and refactor pixel calculation functions

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ TEXT_OUTLINE = 2  # pixels thick (lower looks better close up)
 # pygame setup
 pygame.init()
 # screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen = pygame.display.set_mode(size=(1920, 1080))
+# screen = pygame.display.set_mode(size=(1920, 1080)) #full HD
+screen = pygame.display.set_mode(size=(1366, 768))  # regular HD
+
 clock = pygame.time.Clock()
 running = True
 
@@ -66,7 +68,7 @@ message_number = 0
 escape_count = 0
 
 
-def pixels_per_point() -> float:
+def calculate_pixels_per_point() -> float:
     "Calculate pixels / points"
     global FONT_NAME
     font_size_pts = 500
@@ -76,7 +78,7 @@ def pixels_per_point() -> float:
     return height / font_size_pts
 
 
-font_scale = pixels_per_point()
+pixels_per_point = calculate_pixels_per_point()
 
 
 def draw_background() -> None:
@@ -86,11 +88,11 @@ def draw_background() -> None:
     screen.fill((20, 20, 20), special_flags=pygame.BLEND_RGB_SUB)  # darken it
 
 
-def draw_text(text: str, font_size: int) -> None:
-    global screen, TEXT_OUTLINE, FONT_NAME, font_scale
-    my_font = pygame.font.SysFont(FONT_NAME, font_size)
+def draw_text(text: str, font_size: int | float) -> None:
+    global screen, TEXT_OUTLINE, FONT_NAME, pixels_per_point
+    my_font = pygame.font.SysFont(FONT_NAME, int(font_size + 0.5))
     lines = text.split("\n")
-    line_height = font_size * font_scale * 1.1
+    line_height = font_size * pixels_per_point * 1.1
     x = screen.get_width() / 2
     y = screen.get_height() / 2 - (len(lines) - 1) * line_height / 2
     for line in lines:
@@ -105,10 +107,11 @@ def draw_text(text: str, font_size: int) -> None:
 
 
 def draw_timer() -> None:
-    global minutes_left, clock_speed
+    global minutes_left, clock_speed, screen, pixels_per_point
     draw_background()
     minutes, seconds = divmod(int(minutes_left * 60), 60)
-    draw_text(f"{minutes}:{seconds:02d}", 400)
+    draw_text(f"{minutes}:{seconds:02d}",
+              screen.get_height() * 0.45 / pixels_per_point)
 
 
 def advance_time():
@@ -147,10 +150,11 @@ def select_hint(event: pygame.event.Event):
 
 
 def draw_hint() -> None:
-    global hints, hint_number
+    global hints, hint_number, screen, pixels_per_point
     draw_background()
     if 1 <= hint_number <= len(hints):
-        draw_text(hints[hint_number - 1], 220)
+        draw_text(hints[hint_number - 1], screen.get_height()
+                  * 0.22 / pixels_per_point)
 
 
 def select_message(event: pygame.event.Event):
